6e816f004654801315a1872703ad450492dc5c6b
Fabien-B <fabien.bonneval@gmail.com>
2022-05-10 23:25:53 +0200

    Crolonome pin layout

--- Crolonome/src/PinLayout.h
+++ Crolonome/src/PinLayout.h
@@ -4,24 +4,31 @@
 #include <HardwareSerial.h>
 #include <Arduino.h>
 
-// display on I2C connector, so we can use the ADC on display pins.
-constexpr uint32_t DISPLAY_CLK = PB6;
-constexpr uint32_t DISPLAY_DIO = PB7;
+
+// color and tirette on I2C connector, so we can free ADC.
+constexpr uint32_t TIRETTE = PB6;
+constexpr uint32_t COLOR = PB7;
+
+constexpr uint32_t R_MEAS = PA7;  // ADC1_IN12, prise tirette
+
+constexpr uint32_t LED = PA6;
+
+// #define UART_TX PA2
+// #define UART_RX PA3
+
+constexpr uint32_t DISPLAY_CLK = PA0;
+constexpr uint32_t DISPLAY_DIO = PA1;
 
 constexpr uint32_t SERVO1 = PB0;
 constexpr uint32_t SERVO2 = PB1;
 constexpr uint32_t SERVO3 = PA8;
 constexpr uint32_t SERVO4 = PA11;
 
-constexpr uint32_t TIRETTE = PA7;
-constexpr uint32_t COLOR = PA6;
+constexpr uint32_t DROPPER = SERVO2;
+constexpr uint32_t TROMPE = SERVO3;
+constexpr uint32_t LANGUE = SERVO4;
 
-constexpr uint32_t EMAG = PB5; // or PB4, PA10, 12, 5
-
-constexpr uint32_t R_MEAS = PA1;    // ADC1_IN6
-
-// #define UART_TX PA2
-// #define UART_RX PA3
+constexpr uint32_t EMAG = PA10;
 
 #define DYNAMIXELS_HALF_DUP_SERIAL Serial1
 
